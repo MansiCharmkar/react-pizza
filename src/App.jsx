ca74a2383d26f1e3fac258c4f19b200c32eea2a8
Mansi Charmkar <mansic632@gmail.com>
2024-12-18 19:08:26 +0530

    Menu design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,25 @@
+import "./global.css";
+
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+const appStyle = {
+  padding: "0 9rem",
+  paddingTop: "4rem",
+  backgroundColor: "#f7f2e9",
+  color: "#27272a",
+};
 function App() {
   // 1. Header
   // 2. Menu
   //    a. PizzaCard
   // 3. Footer
   return (
-    <div>
+    <div style={appStyle}>
       <Header />
       <Menu />
       <Footer />
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div>
-      <h1>Fast React Pizza Co.</h1>
-    </div>
-  );
-}
-
-function Menu() {
-  return (
-    <div>
-      <h2>Our Menu</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint et quod
-        alias eius at illum, quidem libero cupiditate architecto delectus?
-      </p>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <div className="pizza__img">
-        <img src="./pizzas/focaccia.jpg" alt="pizza01" />
-      </div>
-      <div className="details">
-        <p className="name">PizzaName</p>
-        <p className="ingredients">ing01, ing02, ing03</p>
-        <p className="pizzaCount">0</p>
-      </div>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <div>
-      <p>we&apos;r open untill 20:00. Come Visit and Order Online.</p>
-      <button>Order Now</button>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>we&apos;r open untill 20:00. Come Visit and order online.</p>
+      <button>Order Now</button>
+    </div>
+  );
+}
+export default Footer;
